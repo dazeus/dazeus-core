7ef57b6fd16c321de855fe1e6b59be9263cbeed9
Sjors Gielen <dazjorz@dazjorz.com>
2010-10-12 02:06:31 +0200

    Added proper loading of a configuration file.

--- config.cpp
+++ config.cpp
@@ -5,6 +5,10 @@
 
 #include "config.h"
 
+#include <QtCore/QFile>
+#include <QtCore/QDebug>
+#include <QtCore/QStringList>
+
 /**
  * @brief Constructor
  *
@@ -17,7 +21,6 @@ Config::Config()
 , error_( "" )
 , settings_( 0 )
 {
-  settings_ = new QSettings("./dazeus.conf", QSettings::IniFormat);
 }
 
 /**
@@ -47,15 +50,35 @@ const QString &Config::lastError()
  */
 bool Config::loadFromFile( QString fileName )
 {
+  // TODO do this properly in reset()!
   oldNetworks_ = networks_;
   networks_.clear();
   error_ = "";
+  delete settings_;
+  settings_ = 0;
 
   emit beforeConfigReload();
 
   // load!
-  if( 0 )
+  error_.clear();
+  if( !QFile::exists(fileName) )
   {
+    error_ = "Configuration file does not exist: " + fileName;
+  }
+  if( error_.isEmpty() )
+  {
+    settings_ = new QSettings(fileName, QSettings::IniFormat);
+    if( settings_->status() != QSettings::NoError )
+    {
+      error_ = "Could not read configuration file: " + fileName;
+    }
+  }
+
+  if( !error_.isEmpty() )
+  {
+    qWarning() << "Error: " << error_;
+    delete settings_;
+    settings_ = 0;
     emit configReloadFailed();
     return false;
   }
@@ -66,70 +89,82 @@ bool Config::loadFromFile( QString fileName )
 
 const QList<NetworkConfig*> &Config::networks()
 {
+  Q_ASSERT( settings_ != 0 );
   // TODO remove later
   if( networks_.count() > 0 )
     return networks_;
-  ServerConfig *sc;
-  NetworkConfig *nc;
 
-  // Temporary network config for localhost
-  nc = new NetworkConfig;
-  nc->name = "localhost";
-  nc->displayName = "Localhost";
-  nc->autoConnect = true;
-  nc->nickName = nickName_;
-  sc = new ServerConfig;
-  sc->host = "localhost";
-  sc->port = 6667;
-  sc->priority = 5;
-  sc->ssl = false;
-  sc->network = nc;
-  nc->servers.append( sc );
-  networks_.append( nc );
+  qDebug() << "Keys in settings: " << settings_->allKeys();
+  qDebug() << "Child keys: " << settings_->childKeys();
+  qDebug() << "Child groups:  " << settings_->childGroups();
 
-  // Temporary network config for Tweakers
-  nc = new NetworkConfig;
-  nc->name = "tweakers";
-  nc->displayName = "Tweakers";
-  nc->autoConnect = true;
-  nc->nickName = nickName_;
-  sc = new ServerConfig;
-  sc->host = "irc.tweakers.net";
-  sc->port = 6667;
-  sc->priority = 5;
-  sc->ssl = false;
-  sc->network = nc;
-  nc->servers.append( sc );
-  networks_.append( nc );
+  QString defaultNickname = settings_->value("generic/nickname").toString();
+  QHash<QString,NetworkConfig*> networks;
+  QMultiHash<QString,ServerConfig*> servers;
+  foreach( const QString &category, settings_->childGroups() )
+  {
+    if( category.toLower().startsWith("network") )
+    {
+      QString networkName = category.mid(8).toLower();
+      qDebug() << "Network: " << networkName;
+      if( servers.contains(networkName) )
+      {
+        qWarning() << "Warning: Two network blocks for " << networkName
+                   << "exist in your configuration file."
+                      " Behaviour is undefined.";
+      }
 
-  // Temporary network config for Freenode
-  nc = new NetworkConfig;
-  nc->name = "freenode";
-  nc->displayName = "FreeNode";
-  nc->autoConnect = true;
-  nc->nickName = nickName_;
-  sc = new ServerConfig;
-  sc->host = "anthony.freenode.net";
-  sc->port = 6667;
-  sc->priority = 5;
-  sc->ssl = false;
-  sc->network = nc;
-  nc->servers.append( sc );
-  sc = new ServerConfig;
-  sc->host = "wolfe.freenode.net";
-  sc->port = 6667;
-  sc->priority = 5;
-  sc->ssl = false;
-  sc->network = nc;
-  nc->servers.append( sc );
-  sc = new ServerConfig;
-  sc->host = "2001:1418:13:1::25";
-  sc->port = 6667;
-  sc->priority = 10;
-  sc->ssl = false;
-  sc->network = nc;
-  nc->servers.append( sc );
-  networks_.append( nc );
+      NetworkConfig *nc = new NetworkConfig;
+      nc->name          = networkName;
+      nc->displayName   = settings_->value(category + "/displayname",
+                                          networkName).toString();
+      nc->autoConnect   = settings_->value(category + "/autoconnect",
+                                          false).toBool();
+      nc->nickName      = settings_->value(category + "/nickname",
+                                          nickName_).toString();
+      networks.insert(networkName, nc);
+    }
+    else if( category.toLower().startsWith("server") )
+    {
+      QString networkName = category.mid(7).toLower();
+
+      if( !networks.contains(networkName) )
+      {
+        qWarning() << "Warning: Server block for " << networkName
+                   << "exists, but no network block found yet. Ignoring block.";
+        continue;
+      }
+
+      ServerConfig *sc = new ServerConfig;
+      sc->host         = settings_->value(category + "/host","").toString();
+      sc->port         = settings_->value(category + "/port",6667).toInt();
+      sc->priority     = settings_->value(category + "/category", 5).toInt();
+      sc->ssl          = settings_->value(category + "/ssl", false).toBool();
+      sc->network      = networks[networkName];
+      networks[networkName]->servers.append( sc );
+
+      qDebug() << "Server for network: " << networkName;
+    }
+    else if( category.toLower() != "generic" )
+    {
+      qWarning() << "Warning: Configuration category name not recognized: "
+                 << category;
+    }
+  }
+
+  QHashIterator<QString,NetworkConfig*> i(networks);
+  while(i.hasNext())
+  {
+    i.next();
+    if( i.value()->servers.count() == 0 )
+    {
+      qWarning() << "Warning: Network block for " << i.key()
+                 << " exists, but no server block found. Ignoring block.";
+      continue;
+    }
+  }
+
+  networks_ = networks.values();
 
   return networks_;
 }
