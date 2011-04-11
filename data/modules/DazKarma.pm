6d9087d3e3607746349de6e41eb6508e94b36df7
Sjors Gielen <dazjorz@dazjorz.com>
2011-04-11 17:28:39 +0200

    Use the right variables, no useless code, <=> instead of cmp

--- data/modules/DazKarma.pm
+++ data/modules/DazKarma.pm
@@ -162,21 +162,19 @@ sub told {
 	} elsif($command eq "karmafight") {
 		return "You don't have dazeus.commands.karma permissions."
 			if(!$p->has("dazeus.commands.karma"));
-		my $thing = lc($rest);
-		if(!defined($thing) or !length($thing)) {
+		if(!$rest) {
 			return "Syntax: karmafight <forwhat> [<forwhat2> [...]]";
 		}
 		# TODO, match "quoted stuff"
-		my @things = @rest;	# THAR I FIXED IT :(
 		my %map = ();
-		foreach $thingy (@things) {
+		foreach $thingy (@rest) {
 			my $karma = $self->get("karma_$thing");
 			$map{$thingy} = (defined($karma)) ? $karma : 0;
 		}
-		@things = sort {$map{$b} cmp $map{$a}} keys %map;
-		my $out =  $things[0]." wins!!!  results:";
+		@rest = sort {$map{$b} <=> $map{$a}} keys %map;
+		my $out =  $rest[0]." wins!!!  results:";
 		my $count = 0;
-		foreach $thingy (@things) {
+		foreach $thingy (@rest) {
 			$count++;
 			$out .= " $count. $thingy ($map{$thingy})";
 		}
