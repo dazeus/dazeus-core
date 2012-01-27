c90731eb3f78b5f37cf398a1440828303f8aa737
Sjors Gielen <dazjorz@dazjorz.com>
2012-01-27 02:13:38 +0100

    Add Perl bindings module

---
+++ bindings/perl/lib/DaZeus.pm
@@ -0,0 +1,344 @@
+package DaZeus;
+
+use strict;
+use warnings;
+use JSON;
+
+my ($HAS_INET, $HAS_UNIX);
+BEGIN {
+	$HAS_INET = eval 'use IO::Socket::INET; 1';
+	$HAS_UNIX = eval 'use IO::Socket::UNIX; 1';
+};
+
+our $VERSION = '1.00';
+
+sub connect {
+	my ($pkg, $socket) = @_;
+
+	my $self = {
+		handlers => {},
+		events => []
+	};
+	bless $self, $pkg;
+
+	if(ref($socket)) {
+		$self->{sock} = $socket;
+	} else {
+		$self->{socketfile} = $socket;
+	}
+
+	return $self->_connect();
+}
+
+sub _connect {
+	my ($self) = @_;
+	if($self->{sock}) {
+		return $self;
+	}
+
+	if($self->{socketfile} =~ /^tcp:(.+):(\d+)$/ || $self->{socketfile} =~ /^tcp:(.+)$/) {
+		if(!$HAS_INET) {
+			die "TCP connection requested, but IO::Socket::INET couldn't be loaded";
+		}
+		my $host = $1;
+		my $port = $2;
+		$self->{sock} = IO::Socket::INET->new(
+			PeerAddr => $host,
+			PeerPort => $port,
+			Proto    => 'tcp',
+			Type     => SOCK_STREAM,
+			Blocking => 0,
+		) or die $!;
+	} elsif($self->{socketfile} =~ /^unix:(.+)$/) {
+		if(!$HAS_UNIX) {
+			die "UNIX connection requested, but IO::Socket::UNIX couldn't be loaded";
+		}
+		my $file = $1;
+		$self->{sock} = IO::Socket::UNIX->new(
+			Peer => $file,
+			Type => SOCK_STREAM,
+			Blocking => 0,
+		) or die $!;
+	} else {
+		die "Didn't understand format of socketfile: " . $self->{socketfile} . " -- does it begin with unix: or tcp:?\n";
+	}
+
+	return $self;
+}
+
+sub networks {
+	my ($self) = @_;
+	$self->_send({get => "networks"});
+	my $response = $self->_read();
+	if($response->{success}) {
+		return $response->{networks};
+	} else {
+		$response->{error} ||= "Request failed, no error";
+		die $response->{error};
+	}
+}
+
+sub channels {
+	my ($self, $network) = @_;
+	$self->_send({get => "channels", params => [$network]});
+	my $response = $self->_read();
+	if($response->{success}) {
+		return $response->{channels};
+	} else {
+		$response->{error} ||= "Request failed, no error";
+		die $response->{error};
+	}
+}
+
+sub message {
+	my ($self, $network, $channel, $message) = @_;
+	$self->_send({do => "message", params => [$network, $channel, $message]});
+	my $response = $self->_read();
+	if($response->{success}) {
+		return 1;
+	} else {
+		$response->{error} ||= "Request failed, no error";
+		die $response->{error};
+	}
+}
+
+sub subscribe {
+	my ($self, @events) = @_;
+	if(ref($events[$#events]) eq "CODE") {
+		my $handler = pop(@events);
+		foreach(@events) {
+			$self->{handlers}{$_} = $handler;
+		}
+	}
+
+	$self->_send({do => "subscribe", params => \@events});
+	my $response = $self->_read();
+	return $response->{added};
+}
+
+sub unsubscribe {
+	my ($self, @events) = @_;
+	$self->_send({do => "unsubscribe", params => \@events});
+	my $response = $self->_read();
+	return $response->{removed};
+}
+
+sub handleEvent {
+	my ($self, $timeout) = @_;
+	if(@{$self->{events}} == 0) {
+		my $packet = $self->_readPacket($timeout);
+		if(!$packet) {
+			return;
+		}
+		if(!$packet->{event}) {
+			die "Error: Out of bound non-event packet received in handleEvent";
+		}
+		push @{$self->{events}}, $packet;
+	}
+	my $event = shift @{$self->{events}};
+	my $handler = $self->{handlers}{$event->{event}};
+	if($handler) {
+		$handler->($self, $event);
+	}
+	return $event;
+}
+
+sub _readPacket {
+	use bytes;
+	my ($self, $timeout) = @_;
+	my $stop_time = time() + $timeout if(defined $timeout);
+	my $event_part;
+	my $size = 10;
+	# A size of 20 bytes is too large, something must be happening
+	while($size < 20 && (!defined($stop_time) || time() < $stop_time)) {
+		undef $event_part;
+		if(!defined($self->{sock}->recv($event_part, $size, MSG_PEEK))) {
+			# Error in peek
+			delete $self->{sock};
+			die $!;
+		}
+		# Has enough data been read?
+		if(length($event_part) == $size && index($event_part, '{') > 0) {
+			# Start of JSON data found
+			my $pre_json_size = index($event_part, '{');
+			my $json_size = substr($event_part, 0, $pre_json_size);
+			# Ignore all non-numerals:
+			$json_size =~ s/[^0-9]//g;
+
+			# See if we can pull all the data from the socket now
+			if(!defined($self->{sock}->recv($event_part, $pre_json_size + $json_size, MSG_PEEK))) {
+				# Error in peek
+				delete $self->{sock};
+				die $!;
+			}
+			if(length($event_part) != ($pre_json_size + $json_size)) {
+				# Try again later
+				next;
+			}
+			# That worked, so now we can actually take it off the socket
+			if(!defined($self->{sock}->recv($event_part, $pre_json_size + $json_size))) {
+				# Error in read
+				delete $self->{sock};
+				die $!;
+			}
+			my $json = decode_json(substr($event_part, $pre_json_size));
+			return $json;
+		} elsif(length($event_part) == $size) {
+			# We read something, but it's not in this response yet
+			$size += 4;
+		}
+	}
+	return;
+}
+
+sub handleEvents {
+	my ($self) = @_;
+	my @events;
+	while(my $event = $self->handleEvent(0)) {
+		push @events, $event;
+	}
+	return @events;
+}
+
+# Read incoming JSON requests until a non-event comes in (blocking)
+sub _read {
+	my ($self) = @_;
+	while(1) {
+		my $packet = $self->_readPacket();
+		if($packet->{'event'}) {
+			push @{$self->{events}}, $packet;
+		} else {
+			return $packet;
+		}
+	}
+}
+
+sub _send {
+	my ($self, $msg) = @_;
+	my $json = encode_json($msg);
+	$self->{sock}->write(bytes::length($json) . $json . "\r\n");
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+DaZeus - Perl binding for DaZeus IRC bot communication
+
+=head1 SYNOPSIS
+
+  use DaZeus;
+  my $d = DaZeus->connect("unix:/tmp/dazeus.sock");
+  # or:
+  my $d = DaZeus->connect("tcp:localhost:1234");
+  
+  # Get connection status
+  my $networks = $d->networks();
+  foreach (@$networks) {
+    my $channels = $d->channels($n);
+    print "$_:\n";
+    foreach (@$channels) {
+      print "  $_\n";
+    }
+  }
+  
+  # Subscribe to events
+  $d->subscribe("JOINED", sub { warn "JOINED event received!" });
+  while(1) {
+    $d->handleEvent() or last;
+  }
+
+=head1 DESCRIPTION
+
+This module is the Perl version of the DaZeus library bindings. They allow a
+Perl application to act as a DaZeus 2 plugin.
+
+=head1 METHODS
+
+=head2 connect($socket)
+
+Creates a DaZeus object connected to the given socket. Returns the object if
+the initial connection succeeded; undef otherwise. If, after this initial
+connection, the connection fails, for example because the bot is restarted, the
+module will re-connect.
+
+The given socket name must either start with "tcp:" or "unix:"; in the case of
+a UNIX socket it must contain a path to the UNIX socket, in the case of TCP it
+may end in a port number. IPv6 addresses must have a port number attached, so
+it can be distinguished from the rest of the address; in "tcp:2001:db8::1:1234"
+1234 is the port number.
+
+If you give an object through $socket, the module will attempt to use it
+directly as the target socket.
+
+=head2 networks()
+
+Returns a list of all networks known in this DaZeus instance.
+
+=head2 channels($network)
+
+Given a network name, returns a list of all channels on this network. If the
+bot does not know about the network, this method calls die().
+
+=head2 message($network, $channel, $message)
+
+Says $message on given channel and network. If the channel is not joined on the
+network, this method calls die().
+
+=head2 subscribe(@events, [$handler])
+
+Subscribes to the given events. Call handleEvent() to handle the next incoming
+event. If the last parameter is a code reference, it is called for every event
+in @events.
+
+=head2 unsubscribe(@events)
+
+Unsubscribes from the given events. Events that already occured before the
+unsubscription, but never handled, may still come in.
+
+=head2 handleEvent($timeout)
+
+Return one event, or return undef if no event comes in within the given timeout
+parameter (in seconds). If an event was received during the last command, it
+will be handled by this method immediately.
+
+If a handler is defined for the incoming event, the handler is called before
+this method returns.
+
+=head2 handleEvents()
+
+Handle all events that can be immediately handled, by calling handleEvent until
+it returns undef, then returning a list of all events it returned.
+
+=head1 AUTHOR
+
+Sjors Gielen, E<lt>dazeus@sjorsgielen.nlE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Sjors Gielen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=cut
---
+++ bindings/perl/t/DaZeus.t
@@ -0,0 +1,101 @@
+use strict;
+use warnings;
+
+use Test::More tests => 12;
+use IO::Socket::UNIX;
+use File::Temp qw(tempdir);
+use JSON;
+BEGIN { use_ok('DaZeus') };
+
+binmode(STDOUT, ':utf8');
+
+my $dir = tempdir(CLEANUP => 1);
+my $socketfile = $dir . "/test.sock";
+my $socket = IO::Socket::UNIX->new(
+	Type => SOCK_STREAM,
+	Local => $socketfile,
+	Listen => 1,
+);
+
+#### Fork to handle the responses
+my $pid = fork();
+if($pid == 0) {
+	# I'm the child process, I'll handle the responses
+	sleep 2;
+	my $otherside = $socket->accept();
+	my $child_dazeus = DaZeus->connect($otherside);
+	my $foo_sub = 0;
+	my $bar_sub = 0;
+	while(my $packet = $child_dazeus->_readPacket(5)) {
+		my $what = $packet->{'get'} || $packet->{'do'};
+		my @params = @{$packet->{'params'}} if($packet->{'params'});
+		if($what eq "networks") {
+			$child_dazeus->_send({got => "networks", "networks" => ["foo"], success => JSON::true});
+		} elsif($what eq "channels") {
+			if(@params == 1 && $params[0] eq "foo") {
+				$child_dazeus->_send({got => "channels", "channels" => ["#bar"], network => "foo", success => JSON::true});
+			} else {
+				$child_dazeus->_send({got => "channels", success => JSON::false, error => "(this test is supposed to fail)"});
+			}
+		} elsif($what eq "message") {
+			if(@params == 3 && $params[0] eq "quux" && $params[1] eq "#mumble" && $params[2] eq "¬Fø") {
+				$child_dazeus->_send({did => "message", success => JSON::true});
+			} else {
+				die "Params to message incorrect: $params[0] $params[1] $params[2]\n";
+			}
+		} elsif($what eq "subscribe") {
+			if(@params == 1 && $params[0] eq "FOO") {
+				die "Double subscribe" if($foo_sub);
+				$foo_sub = 1;
+				$child_dazeus->_send({did => "subscribe", added => 1, success => JSON::true});
+			} elsif(@params == 1 && $params[0] eq "BAR") {
+				die "Double subscribe" if($bar_sub);
+				$bar_sub = 1;
+				$child_dazeus->_send({did => "subscribe", added => 1, success => JSON::true});
+				sleep 2;
+				$child_dazeus->_send({event => "FOO", params => ["p1", "p2"]});
+				$child_dazeus->_send({event => "BAR", params => []});
+			} else {
+				die "Params to subscribe incorrect: $params[0]\n";
+			}
+		}
+	}
+	exit;
+}
+####
+
+my $dazeus;
+eval {
+	$dazeus = DaZeus->connect("unix:" . $socketfile);
+};
+if($@) {
+	fail("connecting");
+	BAIL_OUT("DaZeus module cannot connect to the socketfile.");
+	exit;
+}
+
+pass("connecting");
+ok($dazeus, "connect() returned true");
+ok(ref($dazeus) eq "DaZeus", "got a DaZeus object");
+
+is_deeply($dazeus->networks(), ["foo"], "got the usable networks");
+is_deeply($dazeus->channels("foo"), ["#bar"], "got the usable channels");
+
+ok($dazeus->message("quux", "#mumble", "¬Fø"), "sending a message to IRC");
+
+eval {
+	$dazeus->channels("bar");
+};
+ok($@ =~ /this test is supposed to fail/, "errors are thrown correctly");
+
+my $eventhandled = 0;
+$dazeus->subscribe("FOO", sub { $eventhandled = 1 });
+$dazeus->subscribe("BAR");
+is_deeply($dazeus->handleEvent(), {event => "FOO", params => ["p1", "p2"]}, "Event received correctly");
+ok($eventhandled, "Event handler called correctly");
+$eventhandled = 0;
+is_deeply($dazeus->handleEvent(), {event => "BAR", params => []}, "Event 2 received correctly");
+ok(!$eventhandled, "Event handled not called twice");
+
+diag "Waiting for child to quit...\n";
+waitpid($pid, 0);
