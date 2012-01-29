c9408e6dda7e9ff6419de6e684db360d520012e9
Sjors Gielen <dazjorz@dazjorz.com>
2012-01-29 17:32:17 +0100

    Add WHOIS and NAMES events

--- bindings/perl/examples/monitor.pl
+++ bindings/perl/examples/monitor.pl
@@ -12,7 +12,7 @@ if(!$socket) {
 my $dazeus = DaZeus->connect($socket);
 $dazeus->subscribe(qw/WELCOMED CONNECTED DISCONNECTED JOINED PARTED MOTD
 	QUIT NICK MODE TOPIC INVITE KICK MESSAGE NOTICE CTCPREQ CTCPREPL
-	ACTION NUMERIC UNKNOWN/);
+	ACTION NUMERIC UNKNOWN WHOIS NAMES/);
 
 while(my $event = $dazeus->handleEvent()) {
 	print Dumper($event);
