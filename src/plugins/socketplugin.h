4d2fb99958c1b0ab1a989b5c32845c61602d06fe
Sjors Gielen <dazjorz@dazjorz.com>
2012-02-05 17:35:03 +0100

    Allow plugins to define commands through SocketPlugin, along with their authentication requirements. Limits on network or receiver/sender scope can be made. Identification and authorization can then be done in core; a plugin only needs to listen for incoming "COMMAND" parameters.

--- src/plugins/socketplugin.h
+++ src/plugins/socketplugin.h
@@ -10,6 +10,7 @@
 #include <QtCore/QVariant>
 #include <QtCore/QIODevice>
 #include <QtCore/QByteArray>
+#include <QtCore/QMultiMap>
 #include <QtNetwork/QTcpServer>
 #include <QtNetwork/QLocalServer>
 
@@ -27,7 +28,35 @@ class SocketPlugin : public Plugin
     QString command;
     QString fullArgs;
     QStringList args;
-    Command(Network &n) : network(n) {}
+    bool whoisSent;
+    Command(Network &n) : network(n), whoisSent(false) {}
+  };
+
+  struct RequirementInfo {
+    bool needsNetwork;
+    bool needsReceiver;
+    bool needsSender;
+    Network *wantedNetwork;
+    QString wantedReceiver;
+    QString wantedSender;
+    // Constructor which allows anything
+    RequirementInfo() : needsNetwork(false), needsReceiver(false),
+        needsSender(false), wantedNetwork(0) {}
+    // Constructor which allows anything on a network
+    RequirementInfo(Network *n) : needsNetwork(true), needsReceiver(false),
+        needsSender(false), wantedNetwork(n) {}
+    // Constructor which allows anything from a sender (isSender=true)
+    // or to some receiver (isSender=false)
+    RequirementInfo(Network *n, QString obj, bool isSender) :
+        needsNetwork(true), wantedNetwork(n), needsReceiver(false),
+        needsSender(false)
+    {
+        if(isSender) {
+            needsSender = true; wantedSender = obj;
+        } else {
+            needsReceiver = true; wantedReceiver = obj;
+        }
+    }
   };
 
   struct SocketInfo {
@@ -44,6 +73,33 @@ class SocketPlugin : public Plugin
         return false;
       subscriptions.append(t.toUpper());
       return true;
+    }
+    bool isSubscribedToCommand(const QString &cmd, const QString &recv,
+        const QString &sender, bool identified, const Network &network)
+    {
+        QList<RequirementInfo*> options = commands.values(cmd);
+        foreach(const RequirementInfo *info, options) {
+            if(info->needsNetwork && info->wantedNetwork != &network) {
+                continue;
+            } else if(info->needsReceiver && info->wantedReceiver != recv) {
+                continue;
+            } else if(info->needsSender) {
+                if(!identified || info->wantedSender != sender)
+                    continue;
+            }
+            return true;
+        }
+        return false;
+    }
+    bool commandMightNeedWhois(const QString &cmd) {
+        QList<RequirementInfo*> options = commands.values(cmd);
+        foreach(const RequirementInfo *info, options) {
+            if(info->needsSender) return true;
+        }
+        return false;
+    }
+    void subscribeToCommand(const QString &cmd, RequirementInfo *info) {
+        commands.insert(cmd, info);
     }
     void dispatch(QIODevice *d, QString event, QStringList parameters) {
       Q_ASSERT(!event.contains(' '));
@@ -67,6 +123,7 @@ class SocketPlugin : public Plugin
     }
     QString type;
     QStringList subscriptions;
+    QMultiMap<QString,RequirementInfo*> commands;
     int waitingSize;
   };
 
@@ -128,7 +185,7 @@ class SocketPlugin : public Plugin
     QList<Command*> commandQueue_;
     QMap<QIODevice*,SocketInfo> sockets_;
     void handle(QIODevice *dev, const QByteArray &line, SocketInfo &info);
-    void flushCommandQueue(const QString &nick = QString());
+    void flushCommandQueue(const QString &nick = QString(), bool identified = false);
 };
 
 #endif
