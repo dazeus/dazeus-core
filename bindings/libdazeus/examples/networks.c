9222e4a6f960f97ea0c26235cf52b8afd47e76b5
Sjors Gielen <sjors@sjorsgielen.nl>
2012-02-10 02:19:37 +0100

    Update networks example to also print channels on each network

--- bindings/libdazeus/examples/networks.c
+++ bindings/libdazeus/examples/networks.c
@@ -16,19 +16,31 @@ int main(int argc, char *argv[]) {
 		libdazeus_close(d);
 		return 3;
 	}
-	dazeus_network *net = libdazeus_networks(d);
+	dazeus_stringlist *net = libdazeus_networks(d);
 	if(net == NULL) {
 		fprintf(stderr, "Failed reading DaZeus networks: %s\n", d->error);
 		libdazeus_close(d);
 		return 4;
 	}
 	int i = 0;
-	dazeus_network *neti = net;
+	dazeus_stringlist *neti = net;
 	while(neti != 0) {
-		printf("Network %d: %s\n", ++i, neti->network_name);
+		printf("Network %d: %s\n", ++i, neti->value);
+		dazeus_stringlist *channels = libdazeus_channels(d, neti->value);
+		if(channels == NULL) {
+			fprintf(stderr, "Failed reading DaZeus channels for %s: %s\n", neti->value, d->error);
+			libdazeus_close(d);
+			return 5;
+		}
+		dazeus_stringlist *chani = channels;
+		while(chani != 0) {
+			printf("  Channel: %s\n", chani->value);
+			chani = chani->next;
+		}
+		libdazeus_stringlist_free(channels);
 		neti = neti->next;
 	}
-	libdazeus_networks_free(net);
+	libdazeus_stringlist_free(net);
 	libdazeus_close(d);
 	return 0;
 }
