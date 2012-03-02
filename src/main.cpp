f78118a511535fdd20b7ec67d1f0b7bd7d89c1bf
Sjors Gielen <sjors@sjorsgielen.nl>
2012-03-02 05:24:35 +0100

    Removed all Qt from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
  * See LICENSE for license.
  */
 
-#include <QtCore/QCoreApplication>
-#include <QtCore/QDebug>
 #include "dazeus.h"
 #include "dazeusglobal.h"
 
@@ -12,15 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    qDebug() << "DaZeus version: " << DAZEUS_VERSION_STR;
+    fprintf(stderr, "DaZeus version: %s", DAZEUS_VERSION_STR);
     unsigned int high, low;
     irc_get_version(&high, &low);
     fprintf(stdout, "IRC library version: %d.%02d\n", high, low);
 
     // TODO parse command-line options
-    DaZeus d( QLatin1String("./dazeus.conf") );
+    DaZeus d( "./dazeus.conf" );
 
     // find and initialise plugins
     d.initPlugins();
