7952805138f37dd866fb2208d518e9ba8a4b8ea0
Sjors Gielen <sjors@sjorsgielen.nl>
2012-03-04 01:20:39 +0100

    Add strToLower() function for std::string lowercasing.

---
+++ src/utils.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2012, Sjors Gielen <dazeus@sjorsgielen.nl>
+ * See LICENSE for license.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+std::string strToLower(const std::string &f);
+
+#endif
